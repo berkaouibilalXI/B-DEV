c54b7f11aa1870c1b1098a6a36e905415e3509bc
bilaldeadbeef <berkaouibilal@mail.ru>
2025-02-17 14:18:05 +0100

    refactor: remove unused configuration and prompt files; update package.json for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   assetsInclude: ['**/*.jpg'],
   plugins: [react()],
+  base: '/portfolio',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
