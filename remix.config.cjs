e0dc68a3b1b068fd3b2ba99b856a8efb988dd535
Bilal Berkaoui <berkaouibilal@mail.ru>
2025-05-02 11:05:23 +0000

    Will try

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,9 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-    appDirectory: "app",
-    assetsBuildDirectory: "public/build",
-    publicPath: "/build/",
-    serverBuildPath: "build/index.js",
-    serverModuleFormat: "cjs",
+  serverBuildTarget: "node", // or "node" if using ESM
+  server: "./server.js", // optional, for a custom entry
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildPath: "build/index.js",
+
   };
   
