109124327afc53d2bf3e4b7ddb24373399f7cabb
Bilal Berkaoui <berkaouibilal@mail.ru>
2025-05-02 12:08:51 +0000

    010

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   assetsInclude: ['**/*.jpg'],
   plugins: [react()],
-  base: '/B-DEV/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
