8a2005883fb989236327c7681e7d76380e100588
Berkaoui Bilal <93451498+berkaouibilalXI@users.noreply.github.com>
2025-05-16 11:15:13 +0000

    update: modify dev script to include host option and update GitHub link in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -105,7 +105,7 @@ const Hero = () => {
             className="flex justify-center space-x-4 mb-8"
           >
             {[
-              { Icon: Github, href: 'https://github.com/berkaouibilal' },
+              { Icon: Github, href: 'https://github.com/berkaouibilalXI' },
               { Icon: Mail, href: 'mailto:berkaouibilal@mail.ru' },
             ].map(({ Icon, href }, index) => (
               <motion.a
